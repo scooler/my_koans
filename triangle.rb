03af00e3bad22695fe51d0b9d6538d8836cd2c0b
Pawel Lorek <plorek@neubloc.com>
2011-06-12 00:36:21 +0200

    Triangle done - wow 3rd grade math - guess I'm out of school :P

--- triangle.rb
+++ triangle.rb
@@ -6,7 +6,7 @@
 # It returns:
 #   :equilateral  if all sides are equal
 #   :isosceles    if exactly 2 sides are equal
-#   :scalene      if no sides are equal
+#         if no sides are equal
 #
 # The tests for this method can be found in
 #   about_triangle_project.rb
@@ -14,7 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  error_check(a, b, c)
+
+  case uniq_sides(a, b, c).size
+  when 1
+    return :equilateral
+  when 2
+    return :isosceles
+  else
+    return :scalene
+  end
+end
+
+def uniq_sides(a,b,c)
+  [a, b, c].inject({}){ |h,s| h[s] ||= s; h}.values
+end
+
+def error_check(a, b, c)
+  sorted_sides = [a, b, c].sort
+  
+  raise TriangleError if sorted_sides[0] <= 0
+  
+  raise TriangleError if sorted_sides[2] >= sorted_sides[0] + sorted_sides[1]
 end
 
 # Error class used in part 2.  No need to change this code.
